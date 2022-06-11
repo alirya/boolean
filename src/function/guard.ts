0a99cdaee9b5f0941a381876ad3f725f4e27fd25
dikac <rigel234@yahoo.com>
2022-06-11 07:46:00 +0700

    add inference to guard

--- src/function/guard.ts
+++ src/function/guard.ts
@@ -4,4 +4,6 @@ export default interface Guard<Allow, Expect, Arguments extends unknown[] = []> 
 }
 
 
-
+export type GuardInferAllow<Allow> = Allow extends Guard<infer As, any, any> ? As : never;
+export type GuardInferExpect<Expect> = Expect extends Guard<any, infer As, any> ? As : never;
+export type GuardInferArguments<Arguments> = Arguments extends Guard<any, any, infer As> ? As : never;
