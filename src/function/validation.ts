c60965595adddc61017242745a7f3db40330e56e
dikac <rigel234@yahoo.com>
2023-01-31 19:06:07 +0700

    ref: improve validation

--- src/function/validation.ts
+++ src/function/validation.ts
@@ -2,3 +2,11 @@ export default interface Validation<Arguments extends unknown[], Return extends 
     (...arg: Arguments) : Return;
 }
 
+export interface ValidationAsync<Arguments extends unknown[], Return extends boolean = boolean> {
+    (...arg: Arguments) : Promise<Return>;
+}
+
+export interface ValidationAsyncable<Arguments extends unknown[], Return extends boolean = boolean> {
+    (...arg: Arguments) : Return|Promise<Return>;
+}
+
